0ebbc52dd2b317e8e8f57253144cf52dcac7d354
Emery Hemingway <ehmry@posteo.net>
2023-01-18 17:18:25 -0600

    url_stores: add newSystemStore() and erisDecodeUrls()

--- src/eris/url_stores.nim
+++ src/eris/url_stores.nim
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: MIT
 
 import
-  std / [asyncfutures, uri]
+  std / [asyncdispatch, uri]
 
 import
-  ../eris, ./coap_stores, ./http_stores
+  ../eris, ./coap_stores, ./http_stores, ./composite_stores
 
 proc newStoreClient*(url: Uri): Future[ErisStore] =
   case url.scheme
@@ -15,3 +15,29 @@ proc newStoreClient*(url: Uri): Future[ErisStore] =
   else:
     result = newFuture[ErisStore]("newStoreClient")
     result.fail newException(ValueError, "unsupported URL scheme")
+
+when defined(posix):
+  import
+    os
+
+  from strutils import split
+
+  proc newSystemStore*(): Future[MultiStore] {.async.} =
+    ## Create a new `ErisStore` composed from the store URLs
+    ## listed in the `ERIS_STORE_URL` environment variable.
+    var multi = newMultiStore()
+    let urls = getEnv"ERIS_STORE_URL"
+    for s in split(urls, ' '):
+      if s != "":
+        let
+          u = parseUri(s)
+          store = await newStoreClient(u)
+        add(multi, store)
+    return multi
+
+  proc erisDecodeUrls*(): seq[string] =
+    ## Return a list of URLs for ERIS decoding services.
+    ## This list is taken from the environment variable `ERIS_DECODE_URL`.
+    var s = getEnv("ERIS_DECODE_URL")
+    if s != "":
+      result = split(s, ' ')
